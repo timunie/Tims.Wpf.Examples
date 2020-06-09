ea1d0c7229e62dab4b0543e479e98c432a6c5b25
Tim <tim.uniewski@gmail.com>
2020-06-09 19:24:32 +0200

    BlinkingButton via ViewModel

---
+++ src/MahApps.Metro.Examples/BlinkingButton/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BlinkingButton
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string Name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+        }
+
+        private bool _DoBlink;
+        public bool DoBlink
+        {
+            get { return _DoBlink; }
+            set { _DoBlink = value; RaisePropertyChanged(nameof(DoBlink)); }
+        }
+
+    }
+}
