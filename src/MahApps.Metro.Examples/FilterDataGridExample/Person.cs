db73d51531be8fb88032ff5430aafda5f3689762
Tim <tim.uniewski@gmail.com>
2020-10-27 13:13:03 +0100

    Added Filter Example

---
+++ src/MahApps.Metro.Examples/FilterDataGridExample/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilterDataGridExample
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Age { get; }
+    }
+}
