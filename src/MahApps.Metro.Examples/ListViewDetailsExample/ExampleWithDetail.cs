3aaf3dfbcd50b739dce7d5de3cf0609dc6483ea9
Tim <tim.uniewski@gmail.com>
2021-01-19 17:52:22 +0100

    ListView with Details Example

---
+++ src/MahApps.Metro.Examples/ListViewDetailsExample/ExampleWithDetail.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewDetailsExample
+{
+    public class ExampleWithDetail : ObservableObject
+    {
+        private string _Title;
+        public string Title
+        {
+            get { return _Title; }
+            set { SetProperty(ref _Title, value); }
+        }
+
+        private string _Detail;
+        public string Detail
+        {
+            get { return _Detail; }
+            set { SetProperty(ref _Detail, value); }
+        }
+
+    }
+}
---
+++ src/MahApps.Metro.Examples/ListViewDetailsExample/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ListViewDetailsExample
+{
+    public class ViewModel : ObservableObject
+    {
+        public ObservableCollection<ExampleWithDetail> Examples { get; } = new ObservableCollection<ExampleWithDetail>
+        {
+            new ExampleWithDetail() {Title ="Test 1" , Detail ="This is only visible if this ListViewItem is selected"},
+            new ExampleWithDetail() {Title ="Test 2" , Detail ="This is also only visible if this ListViewItem is selected Like 'Test 1'"},
+            new ExampleWithDetail() {Title ="Test 3" , Detail ="Another Test"},
+        };
+    }
+}
