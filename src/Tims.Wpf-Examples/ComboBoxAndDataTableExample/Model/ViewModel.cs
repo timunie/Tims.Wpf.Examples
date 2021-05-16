2c04cfb7b615c201a66f2f754fff49b3d63a818e
Tim <tim.uniewski@gmail.com>
2021-05-16 14:18:26 +0200

    ComboBox and DataTable Example
    Fill in the DataTable to get the Items populated

---
+++ src/Tims.Wpf-Examples/ComboBoxAndDataTableExample/Model/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBoxAndDataTableExample.Model
+{
+    public class ViewModel 
+    {
+        public ViewModel()
+        {
+            People.Columns.Add("First Name", typeof(string));
+            People.Columns.Add("Last Name", typeof(string));
+            People.Columns.Add("Age", typeof(int));
+        }
+
+        public ObservableCollection<string> Animals { get; } = new ObservableCollection<string>()
+        {
+            "Dog",
+            "Cat",
+            "Ant"
+        };
+
+        public DataTable People { get; } = new DataTable();
+    }
+}
