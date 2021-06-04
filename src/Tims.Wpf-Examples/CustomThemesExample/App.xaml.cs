736f59ed45882139143fb2e691ba14eb9868c414
Tim <tim.uniewski@gmail.com>
2021-06-04 21:13:25 +0200

    CustomThemesExample

---
+++ src/Tims.Wpf-Examples/CustomThemesExample/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using ControlzEx.Theming;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CustomThemesExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            SetAppTheme("Light", Colors.DarkBlue);
+        }
+
+        public static void SetAppTheme(string baseTheme, Color accent)
+        {
+            var theme = new Theme("MyTheme",
+                                 "MyTheme",
+                                 baseTheme ?? ThemeManager.BaseColorLightConst,
+                                 accent.ToString(),
+                                 accent,
+                                 new SolidColorBrush(accent),
+                                 true,
+                                 false);
+
+            var HsvAccent = new HSVColor(accent);
+
+            App.Current.Resources["My.Brushes.Complementary"] 
+                = new SolidColorBrush(new HSVColor(HsvAccent.Hue + 180,
+                                                   HsvAccent.Saturation,
+                                                   HsvAccent.Value).ToColor());
+
+            ThemeManager.Current.ChangeTheme(App.Current, theme);
+        }
+    }
+}
---
+++ src/Tims.Wpf-Examples/CustomThemesExample/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ControlzEx.Theming;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Theming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomThemesExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonApplyTheme_Click(object sender, RoutedEventArgs e)
+        {
+            App.SetAppTheme(ComboBoxBaseTheme.SelectedItem as string, ColorPickerAccent.SelectedColor.Value);
+        }
+    }
+}
